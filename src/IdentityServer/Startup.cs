00b08c2a27405b14bd5f2aa83bf2cd5a368c8859
enrique.loyola <enrique.loyola@everis.com>
2020-09-15 16:04:14 +0100

    refactor

--- src/IdentityServer/Startup.cs
+++ src/IdentityServer/Startup.cs
@@ -1,12 +1,8 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
-
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace IdentityServer
+﻿namespace IdentityServer
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.DependencyInjection;
+
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
